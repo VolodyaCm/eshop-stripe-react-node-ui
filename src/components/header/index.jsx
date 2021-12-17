f2cac48872635dfe6eb28bc4d28f6d60b39f3ad3
Volodymyr Smal-Stotskiy <volodyacm@gmail.com>
2021-12-17 19:15:31 +0200

    Add header component

---
+++ src/components/header/index.jsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom';
+import './header.styles.scss';
+
+const HeaderComponent = () => {
+  return (
+    <nav className='nav-menu container'>
+      <div className="logo">
+        <Link to='/'>NOMAD</Link>
+      </div>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/shop'>Shop</Link></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default HeaderComponent;
